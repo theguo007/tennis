a6058051aab0fdea1640da5272580dbc214aba94
Kevin Guo <kegu@microsoft.com>
2017-07-29 23:26:04 -0500

    added node project. Connected to the database. Initial route at /api/users is up and running

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
+
+var app = express();
+
+var port = 3000;
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/api', users);
+
+app.listen(port, function(){
+	console.log('Server started on port' + port);
+});
